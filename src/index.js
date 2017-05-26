6694d0632cde52c7e4e3b3a83b2297394b625c09
Oleg Slobodskoi <oleg008@gmail.com>
2017-05-26 17:01:38 +0200

    migrate to prop-types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import findIndex from 'lodash/array/findIndex'
 import noop from 'lodash/utility/noop'
